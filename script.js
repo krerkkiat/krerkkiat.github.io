0c7d42acf97605a46b8edabe7e98eb02a49db4ad
Krerkkiat Chusap <krerkkiat@users.noreply.github.com>
2020-06-25 03:54:24 -0400

    feat: add firebase authentication
    The provider is email with link.

--- script.js
+++ script.js
@@ -1,25 +1,57 @@
-(function () {
+// Trigger rough notation.
+function triggerAnnotation() {
     const annotate = RoughNotation.annotate;
     const annotationGroup = RoughNotation.annotationGroup;
     const $ = (t) => document.querySelector(t);
 
-    // export interface RoughAnnotationConfig {
-    //   type: RoughAnnotationType;
-    //   animate?: boolean; // defaults to true
-    //   animationDuration?: number; // defaulst to 1000ms
-    //   animationDelay?: number; // default = 0
-    //   color?: string; // defaults to currentColor
-    //   strokeWidth?: number; // default based on type
-    //   padding?: number; // defaults to 5px
-    // }
+    // top
+    const a1 = annotate($('section a.abox'), {type: 'box', color: '#F44336', padding: 3});
+    const a2 = annotate($('header h1 span'), {type: 'strike-through', color: '#1B5E20', strokeWidth: 4});
+    // const a3 = annotate($('header a'), { type: 'underline', color: '#2196F3', padding: 3, strokeWidth: 3 });
+    // const a4 = annotate($('header span.acircle'), { type: 'circle', color: '#F44336', padding: 5 });
+    const ag = annotationGroup([a1, a2]);
+    ag.show();
+}
 
-    {
-        // top
-        const a1 = annotate($('section a.abox'), {type: 'box', color: '#F44336', padding: 3});
-        const a2 = annotate($('header h1 span'), { type: 'strike-through', color: '#1B5E20', strokeWidth: 4 });
-        // const a3 = annotate($('header a'), { type: 'underline', color: '#2196F3', padding: 3, strokeWidth: 3 });
-        // const a4 = annotate($('header span.acircle'), { type: 'circle', color: '#F44336', padding: 5 });
-        const ag = annotationGroup([a1, a2]);
-        ag.show();
-    }
-})();
+function initApp() {
+    triggerAnnotation();
+    var uiConfig = {
+        callbacks: {
+            signInSuccessWithAuthResult: (authResult, redirectUrl) => {
+                return true;
+            },
+            uiShown: () => {
+                document.getElementById('loader').style.display = 'none';
+            },
+        },
+        signInSuccessUrl: '#',
+        signInOptions: [
+            {
+                provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+                signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD
+            }
+        ],
+    };
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            document.getElementById('signed-in').style.display = 'inline';
+            document.getElementById('signed-out').style.display = 'none';
+            document.getElementById('loader').style.display = 'none';
+            document.getElementById('sign-out-btn').style.display = 'inline';
+            document.getElementById('sign-out-btn').addEventListener('click', (e) => {
+                firebase.auth().signOut().then(function () {
+                    // Sign-out successful.
+                }).catch(function (error) {
+                    // An error happened.
+                });
+            });
+        } else {
+            document.getElementById('signed-in').style.display = 'none';
+            document.getElementById('signed-out').style.display = 'inline';
+            document.getElementById('sign-out-btn').style.display = 'none';
+            ui.start('#firebaseui-auth-container', uiConfig);
+        }
+    });
+}
+
+window.onload = initApp();
