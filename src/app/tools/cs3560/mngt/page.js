e29cd090751d067c780881e22e5f7a3b073af47c
Krerkkiat Chusap <krerkkiat@users.noreply.github.com>
2023-09-18 13:38:21 -0400

    fix: escape a single quote

--- src/app/tools/cs3560/mngt/page.js
+++ src/app/tools/cs3560/mngt/page.js
@@ -8,7 +8,9 @@ export default function ManagementTool() {
       <div className="flex flex-row justify-center grow bg-violet-50 p-10 rounded-lg w-full md:w-3/4 lg:1/2">
         <div className="w-full py-12">
           <h1 className="text-2xl py-3">Management Tool</h1>
-          <p>Manage students' detail, teams, and checkpoint presentation.</p>
+          <p>
+            Manage students&apos; detail, teams, and checkpoint presentation.
+          </p>
           <div className="mt-8">
             <div className="grid grid-cols-1 gap-6"></div>
           </div>
